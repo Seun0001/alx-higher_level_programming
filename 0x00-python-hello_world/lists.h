143dd44eced2c0cadc58793175b6047fcf580704
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-09-19 07:43:12 +0100

    Header File

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H
+#define _LISTS_H
 
 #include <stdlib.h>
 
@@ -22,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif/*_LISTS_H*/
