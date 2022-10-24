9b044db1a5e42b39cb1daa028fe3f455c664af1d
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-10-24 20:51:32 +0100

    Pass

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,8 +16,8 @@ class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
         """ size init"""
-        super().__init__(self.__size, self.__size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+        return("[Square] " + str(self.__size) + "/" + str(self.__size)
