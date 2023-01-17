1dc781b9062c9b8c9d0211f53d2debfc9d6273f1
Seun0001 <oluwaseunafolabi941@gmail.com>
2023-01-17 17:56:09 +0000

    Done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Check status"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
-def status():
-    """status"""
-    result = requests.get("https://intranet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
-
 if __name__ == "__main__":
-    status()
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Check status"""
+"""
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import requests
-import sys
+from sys import argv
 
-
-def post():
-    """status"""
-    result = requests.post(sys.argv[1], data={"email": sys.argv[2]})
-
-    print(result.text)
 
 if __name__ == "__main__":
-    post()
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
